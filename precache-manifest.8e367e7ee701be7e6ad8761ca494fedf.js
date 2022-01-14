55e551c9ac918e6ba4ccaa427a5b4c9fd8566659
Code Sandbox <hello@codesandbox.io>
2022-01-14 17:16:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8e367e7ee701be7e6ad8761ca494fedf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbbffaaa21b16eb0c4fc5898b44a831",
+    "url": "https://ryansolar14.github.io/csb-11h5p/index.html"
+  },
+  {
+    "revision": "c0e544843cfa412027e8",
+    "url": "https://ryansolar14.github.io/csb-11h5p/static/js/2.97bd378f.chunk.js"
+  },
+  {
+    "revision": "42b802ce6cd7569db937",
+    "url": "https://ryansolar14.github.io/csb-11h5p/static/js/main.16ac5906.chunk.js"
+  },
+  {
+    "revision": "a9be3058fba0b65b676e",
+    "url": "https://ryansolar14.github.io/csb-11h5p/static/js/runtime-main.5a5e55ea.js"
+  }
+]);
